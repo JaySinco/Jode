4181056a99119b524e25c33507b28e9d25a2b785
jaysinco <jaysinco@163.com>
2021-03-08 23:05:52 +0800

    fix bash complete

--- source_in_bash_profile.sh
+++ source_in_bash_profile.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-_build_completions()
-{
-    if [ -d "dest" ]; then
-        COMPREPLY+=($(compgen -W "$(find dest -maxdepth 1 -name *.vcxproj | grep -v [[:upper:]] | awk -F [/.] '{print $2}')" -- "${COMP_WORDS[-1]}"))
-        COMPREPLY+=($(compgen -W "clean" -- "${COMP_WORDS[-1]}"))
-    fi
-}
-
-complete -F _build_completions build.sh
-
 export DOWNLOAD="curl -LJ" # -x socks5h://localhost:8172
 export CMAKE_CMD="cmake -A x64 -G" # -T clangcl
 export CMAKE_GENERATOR="Visual Studio 16 2019"
@@ -22,3 +12,13 @@ export MSVC_BUILD_TYPE="Release" # Debug or Release
 export MSVC_BUILD="MSBuild.exe -p:Configuration=${MSVC_BUILD_TYPE} -maxcpucount *.sln"
 export MSVC_INSTALL="MSBuild.exe -p:Configuration=${MSVC_BUILD_TYPE} INSTALL.vcxproj"
 export PATH="${MSVC_PATH};${PATH}"
+
+_build_completions()
+{
+    if [ -d "${CMAKE_OUT_DIR}" ]; then
+        COMPREPLY+=($(compgen -W "$(find ${CMAKE_OUT_DIR} -maxdepth 1 -name *.vcxproj | grep -v [[:upper:]] | awk -F [/.] '{print $2}')" -- "${COMP_WORDS[-1]}"))
+        COMPREPLY+=($(compgen -W "clean" -- "${COMP_WORDS[-1]}"))
+    fi
+}
+
+complete -F _build_completions build.sh
